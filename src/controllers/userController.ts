ff6f94b97f0f43f64081857abd9171ae3a24cb15
Anna Myronova <annamyronova8@gmail.com>
2025-07-10 16:39:17 +0300

    created authController, userController and middlewareAuth

---
+++ src/controllers/userController.ts
@@ -0,0 +1,72 @@
+import { Request, Response } from "express";
+import * as UserModel from "../models/userModel";
+import { email } from "zod";
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    const user = await UserModel.getUserById(id);
+
+    if (!user) {
+      res.status(404).json({ message: "User is not found" });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    console.error("Error in getUserById controller:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    if (isNaN(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+
+    const deletedUser = await UserModel.deleteUserById(id);
+
+    if (!deletedUser) {
+      res.status(404).json({ message: "User is not found" });
+      return;
+    }
+    res.status(200).json({ message: "User deleted successfully", deletedUser });
+  } catch (err) {
+    console.error("Error in deleteUserById controller:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+    const { email, username } = req.body;
+
+    if (isNaN(id)) {
+      res.status(400).json({ message: "Invalid ID" });
+      return;
+    }
+    if (!username || !email) {
+      return res
+        .status(400)
+        .json({ message: "Username and email are required" });
+    }
+    const updatedUser = await UserModel.updateUser(id, username, email);
+
+    if (!updatedUser) {
+      res.status(404).json({ message: "User is not found" });
+      return;
+    }
+    res.status(200).json({ message: "User updated successfully", updatedUser });
+  } catch (err) {
+    console.error("Error in updateUser controller:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
---
+++ src/middlewareAuth.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWTPayload } from "./types/typeJWTPayload";
+
+const JWT_SECRET = process.env.JWT_SECRET || "secret-key";
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JWTPayload;
+    req.user = { id: decoded.id, email: decoded.email, username: decoded.username };
+    console.log(req.user);
+    console.log("authMiddleware running");
+    next();
+  } catch (error: any) {
+    if (error.name === "TokenExpiredError") {
+      res.status(401).json({ message: "Token expired" });
+      return;
+    }
+    res.status(401).json({ message: "Not valid or expired token" });
+  }
+};
